3f986b99033c479abe83c37464f715a03ebb8441
Kumodya Thamadi <pasindu@slgeek.lk>
2026-01-07 22:42:50 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js"
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT ;
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port - " + PORT);
+    // connectDB();
+    // In startServer function, before connectDB
+console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI);
+});
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+// Auth routes
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+// Profile update
+router.put("/update-profile", protectRoute, updateProfile);
+
+export default router;
